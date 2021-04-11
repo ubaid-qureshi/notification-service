23e6dd45c3844c319554090ae2f7722ce5bdc36b
Ubaid Qureshi <ubaid@tesseract.in>
2021-04-11 15:01:26 +0530

    Added proper logs for controller/user

--- src/controller/User.js
+++ src/controller/User.js
@@ -2,13 +2,14 @@ const { AppError } = require('../utils/requestHandlers/errorHandler');
 
 class User {
   constructor() {
-    this.devDao = require('../model/Dao/User');
+    this.userDao = require('../model/Dao/User');
   }
 
   // User Creation
   async create(userData) {
     try {
-      return await this.devDao.create(userData);
+      _logger.debug(userData);
+      return await this.userDao.create(userData);
     } catch (error) {
       throw new AppError(error.code || 400, error.message);
     }
@@ -34,7 +35,7 @@ class User {
         ...(city && { city }),
       };
       _logger.debug('updateData', updateData);
-      return this.devDao.findOneAndUpdate(id, updateData);
+      return this.userDao.findOneAndUpdate(id, updateData);
     } catch (error) {
       throw new AppError(
         error.message.statusCode || error.code || 400,
@@ -45,27 +46,32 @@ class User {
 
   // Getting Users
   async getOne(id) {
-    return this.devDao.getOne(id);
+    _logger.debug(id);
+    return this.userDao.getOne(id);
   }
 
   async getOneByQuery(filter) {
-    return this.devDao.getOneByQuery(filter);
+    _logger.debug(filter);
+    return this.userDao.getOneByQuery(filter);
   }
 
   async getAllByQuery() {
-    return this.devDao.getAllByQuery({});
+    return this.userDao.getAllByQuery({});
   }
 
+  // Soft deleting user
   async deleteOne(id) {
     try {
       const updatedData = {
         isDeleted: true,
         deletedAt: new Date(),
       };
-      const user = await this.devDao.updateOne(
+      _logger.debug(updatedData);
+      const user = await this.userDao.updateOne(
         id, updatedData,
       );
       if (!user) throw new AppError(404, 'User doesn\'t exist');
+      _logger.debug(user);
       return user;
     } catch (error) {
       throw new AppError(error.code || 400, error.message);
